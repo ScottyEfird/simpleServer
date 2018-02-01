d2e88d32fa3cc5dd3578b5de0bff107a3ca1cb53
Scotty Efird <scottyefird@gmail.com>
2018-01-31 18:57:51 -0600

    Added routes

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express')
 const http = require('http')
 const bodyParser = require('body-Parser')
 const morgan = require('morgan')
-
+const router = require('./router')
 const app = express()
 
 // Express Middleware
 app.use(morgan('combined'))
 app.use(bodyParser.json({ type: '*/*' }))
+router(app)
 
 // Server Setup
 const port = process.env.PORT || 3090
